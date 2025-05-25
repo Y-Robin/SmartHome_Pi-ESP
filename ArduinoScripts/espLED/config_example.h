4303a4b206332ecbb5cf2ae33a6372ed0c5102c0
Yannick Robin <yannick.robin@web.de>
2025-05-25 15:23:52 +0200

    Added example config files for arduino Added lightbar for esp32 aigain Signed-off-by: Yannick Robin <yannick.robin@web.de>

---
+++ ArduinoScripts/espLED/config_example.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "****";
+const char* password = "****";
+const char* serverUrl = "http://xxx.xxx.xxx.xx:80/record_temperature";  // Zielserver
+
+#endif
